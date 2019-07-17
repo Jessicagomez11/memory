ade5f3ab05bf80b4c1fa5003c26551d071b1b0a8
jessica <jessicagomez11@live.com>
2019-07-17 10:42:08 -0700

    Updates

---
+++ precache-manifest.8d73390168341d65bdede84e0cae49f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d91bd930995359bb7b53761063a7613e",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "e05665f85426633afb07",
+    "url": "/memory/static/css/main.c268b958.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "e05665f85426633afb07",
+    "url": "/memory/static/js/main.1a2aa7f7.chunk.js"
+  },
+  {
+    "revision": "174810366761c26401f3",
+    "url": "/memory/static/js/runtime~main.031dcd1e.js"
+  }
+]);
